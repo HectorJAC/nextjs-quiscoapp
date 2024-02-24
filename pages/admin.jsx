93ad40ebf83bd6b78c7e737ced2610afbb8945ec
Hector <hjac10@hotmail.com>
2024-02-23 21:45:43 -0400

    Terminando con la pagina de las ordenes

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -1,17 +1,27 @@
 import useSWR from "swr";
 import axios from "axios";
 import AdminLayout from "../layout/AdminLayout";
+import Orden from "../components/Orden";
 
 export default function Admin() {
     
     const fetcher = () => axios.get('/api/ordenes').then(datos => datos.data);
-    const { data, error, isLoading } = useSWR('/api/ordenes', fetcher);
-    console.log(data);
+    const { data, error, isLoading } = useSWR('/api/ordenes', fetcher, {refreshInterval: 500});
+
 
     return (
         <AdminLayout pagina={'Admin'} >
             <h1 className="text-4xl font-black">Panel de Administración</h1>
             <p className="text-2xl my-10">Administra las Ordenes</p>
+
+            {
+                data && data.length ? data.map((orden) =>
+                    <Orden 
+                        key={orden.id}
+                        orden={orden}
+                    />
+                ) : <p>No hay ordenes pendientes</p>
+            }
         </AdminLayout>
     )
 };
